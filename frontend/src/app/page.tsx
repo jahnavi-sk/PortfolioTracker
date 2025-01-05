615daebc79fc528f9660069b86d52850764028b4
jahnavi-sk <jahnavi31blr@gmail.com>
2025-01-05 23:56:17 +0530

    login sign up ui done

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,208 @@
+"use client";
+import React, {useState} from "react";
+import { SparklesCore } from "../components/ui/sparkles";
+import { EvervaultCard, Icon } from "../components/ui/evervault-card";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { cn } from "@/lib/utils";
+import {
+  IconBrandGithub,
+  IconBrandGoogle,
+  IconBrandOnlyfans,
+} from "@tabler/icons-react";
+ 
+
+export default function SparklesPreview() {
+  const [view, setView] = useState('initial');
+
+  const handleClose = () => {
+    setView('initial');
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  const BottomGradient = () => {
+    return (
+      <>
+        <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+        <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+      </>
+    );
+  };
+  const LabelInputContainer = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    return (
+      <div className={cn("flex flex-col space-y-2 w-full", className)}>
+        {children}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+    
+
+    {view === 'initial' && (
+        <>
+          <div className="mt-2 h-[18rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h3 className="md:text-7xl text-3xl lg:text-7xl font-bold text-center text-white relative z-20">
+              Welcome !
+            </h3>
+            
+            <div className="w-[40rem] h-40 relative">
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+              <SparklesCore
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#FFFFFF"
+              />
+              <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+          </div>
+
+          <div className="flex gap-40 justify-center">
+            <div 
+              onClick={() => setView('login')}
+              className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start w-[25rem] p-4 relative h-[25rem] transition-transform transform click:scale-110 hover:scale-105 cursor-pointer"
+            >
+              <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+              <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+              <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+              <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+              <EvervaultCard text="Log In"/>
+            </div>
+            
+            <div 
+              onClick={() => setView('signup')}
+              className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start w-[25rem] p-4 relative h-[25rem] transition-transform transform hover:scale-105 cursor-pointer"
+            >
+              <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+              <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+              <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+              <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+              <EvervaultCard text="Sign Up" />
+            </div>
+          </div>
+        </>
+      )}
+
+  
+    {view === 'signup' && (
+    <div id="signupbox" className="mt-32 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
+      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Hey, nice to meet you !
+      </h2>
+ 
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="firstname">First name</Label>
+            <Input id="firstname" placeholder="Tyler" type="text"  />
+          </LabelInputContainer>
+          
+          <LabelInputContainer>
+            <Label htmlFor="lastname">Last name</Label>
+            <Input id="lastname" placeholder="Durden" type="text" />
+          </LabelInputContainer>
+        </div>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Confirm Password</Label>
+          <Input
+            id="twitterpassword"
+            placeholder="••••••••"
+            type="twitterpassword"
+          />
+        </LabelInputContainer>
+ 
+        <button
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Sign up &rarr;
+          <BottomGradient />
+        </button>
+
+        <button
+          className="mt-5 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+          onClick={handleClose}
+        >
+          Back &larr;
+          <BottomGradient />
+        </button>
+ 
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+ 
+        
+      </form>
+    </div>
+    )}
+    
+    {view === 'login' && (
+    <div id="loginbox" className="mt-48 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
+      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Hey, nice to see you again !
+      </h2>
+      
+ 
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="firstname">Username</Label>
+            <Input id="firstname" placeholder="Tyler" type="text"  />
+          </LabelInputContainer>
+        </div>
+        
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+    
+        <button
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Log In &rarr;
+          <BottomGradient />
+        </button>
+
+        <button
+          className="mt-5 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+          onClick={handleClose}
+        >
+          Back &larr;
+          <BottomGradient />
+        </button>
+ 
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+ 
+        
+      </form>
+    </div>
+    )}
+    </div>
+  );
+}
+
